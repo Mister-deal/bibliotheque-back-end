de7b559157b60cc1f7a5df3b8e835fcb0802ea67
Othman <120719388+Othman59@users.noreply.github.com>
2025-07-23 14:23:02 +0200

    ajout entité livre

--- bibliotheque-back-end/Models/Livre.cs
+++ bibliotheque-back-end/Models/Livre.cs
@@ -9,6 +9,15 @@ namespace bibliotheque_back_end.Models
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public EtatLivre Etat { get; set; }
 
+        public int Id { get; set; }  // Identifiant unique du livre
+
+        public required string Titre { get; set; }  // Titre du livre
+        public required string Auteur { get; set; }  // Nom de l’auteur
+        public string? Description { get; set; }  // Description du livre
+        public required int AnneePublication { get; set; }  // Année de publication
+        public required string Editeur { get; set; }  // Nom de l'éditeur
+
+
         public ICollection<EmpruntLivre> EmpruntLivres { get; set; }
         public ICollection<Reservation> Reservations { get; set; }
     }
