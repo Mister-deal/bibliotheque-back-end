f836bb02f699e3136f5ae2488fb8ec441785ea19
Vincent <faivre.vinz@gmail.com>
2025-07-23 11:35:17 +0200

    <feature>(Employe, Membre, Emprunt): Add models Employe, Membre, Emprunt.

--- bibliotheque-back-end/Models/Membre.cs
+++ bibliotheque-back-end/Models/Membre.cs
@@ -2,6 +2,13 @@
 {
     public class Membre
     {
+        // -- Les propriétés de l'entité Membre --
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public string MotDePasse { get; set; }
+        
         public ICollection<Emprunt> emprunts { get; set; }
         public ICollection<Reservation> reservations { get; set; }
     }
