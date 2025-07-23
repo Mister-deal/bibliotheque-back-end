8ad72fde9efa83777baf1a76eaa64f5c27762a87
Othman59 <othman.douz07@gmail.com>
2025-07-23 11:55:44 +0200

    Feature: ajout entité livre

--- bibliotheque-back-end/Models/Livre.cs
+++ bibliotheque-back-end/Models/Livre.cs
@@ -1,11 +1,21 @@
-﻿namespace bibliotheque_back_end.Models
+﻿using System.Collections.Generic;
+
+namespace bibliotheque_back_end.Models
 {
     public class Livre
     {
-        public Categorie Categorie { get; set; }
-        public EtatLivre Etat { get; set; }
+        public int Id { get; set; }  // Identifiant unique du livre
 
-        public ICollection<EmpruntLivre> EmpruntLivres { get; set; }
-        public ICollection<Reservation> Reservations { get; set; }
+        public required string Titre { get; set; }  // Titre du livre
+        public required string Auteur { get; set; }  // Nom de l’auteur
+        public string? Description { get; set; }  // Description du livre
+        public required int AnneePublication { get; set; }  // Année de publication
+        public required string Editeur { get; set; }  // Nom de l'éditeur
+
+        public Categorie Categorie { get; set; }  // Navigation vers la catégorie
+        public EtatLivre Etat { get; set; }  // État du livre Enum disponible emprunté, réservé, endommagé, etc. 
+
+        public ICollection<EmpruntLivre> EmpruntLivres { get; set; } = new List<EmpruntLivre>();
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
     }
 }
