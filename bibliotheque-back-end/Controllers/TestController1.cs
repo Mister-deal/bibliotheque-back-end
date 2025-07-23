1e9bacc6649aacd50d2d0dbc6e7f72afc0878ee4
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-07-23 11:28:46 +0200

    feat: installation Swagger and Api Controller Test

---
+++ bibliotheque-back-end/Controllers/TestController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bibliotheque_back_end.Controllers
+{
+    // Pour test TODO: à retirer avant release !
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Ça marche !");
+        }
+    }
+}
