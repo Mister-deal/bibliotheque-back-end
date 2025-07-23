6e8edf5afe99a7ddaf6d2948c2b6a212fb8608f0
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-07-23 17:41:08 +0200

    feat: added 2 controller for checking

---
+++ bibliotheque-back-end/Controllers/LivresController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bibliotheque_back_end.Controllers
+{
+    public class LivresController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Gestion des Livres";
+            return View();
+        }
+    }
+}
