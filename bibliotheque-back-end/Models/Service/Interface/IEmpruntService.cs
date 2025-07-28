0600ab3168b933b6e3afcfb802a27c06f114581c
Mister-deal <max-94400@hotmail.fr>
2025-07-28 16:41:52 +0200

    ajout password.txt gitignore + IEmpruntService.cs

--- bibliotheque-back-end/Models/Service/Interface/IEmpruntService.cs
+++ bibliotheque-back-end/Models/Service/Interface/IEmpruntService.cs
@@ -2,5 +2,14 @@
 
 public interface IEmpruntService
 {
+    IEnumerable<Emprunt> GetAllEmprunts();
+    IEnumerable<Emprunt> GetEmpruntsByMembreId(int membreId);
+    IEnumerable<Emprunt> GetEmpruntsByLivreId(int livreId);
+    Emprunt GetEmpruntById(int id);
+    IEnumerable<Emprunt> GetActiveEmprunts();
     
+    Emprunt CreateEmprunt(int membreId, List<int> livreIds, DateOnly dateRetour, int employeValidationId);
+    Emprunt ReturnSpecificBookFromEmprunt(int empruntId, int livreId, int employeValidationId); // Retourne un livre spécifique d'un emprunt
+    Emprunt DeleteEmprunt(int id);
+    Emprunt EmpruntExists(int id);
 }
