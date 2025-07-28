e890cf7a8ea055f52ef9035dd0458c4adbe49282
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-07-28 12:44:14 +0200

    feat: home page and admin redirect
    add buttons to navigate to admin or home page

--- bibliotheque-back-end/Program.cs
+++ bibliotheque-back-end/Program.cs
@@ -86,6 +86,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
