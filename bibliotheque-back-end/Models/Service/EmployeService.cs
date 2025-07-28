eebc43ba78a3b67722bad20cd906207cfd51a1d8
Mister-deal <max-94400@hotmail.fr>
2025-07-28 16:01:32 +0200

    ajout logique boilerplate services et ajout base repositories services

--- bibliotheque-back-end/Models/Service/EmployeService.cs
+++ bibliotheque-back-end/Models/Service/EmployeService.cs
@@ -1,6 +1,44 @@
 ﻿namespace bibliotheque_back_end.Models.Service.Interface;
 
-public class EmployeService
+public class EmployeService: IEmployeService
 {
-    
+    public IEnumerable<Employe> GetAllEmployees()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe GetEmployeeById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe GetEmployeeByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe AddEmployee(Employe newEmployee, string dataPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe UpdateEmployee(int id, Employe updatedEmployee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe UpdateEmployeeRole(int id, Role updatedRole)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe DeleteEmployee(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool employeeExists(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- bibliotheque-back-end/Models/Service/MembreService.cs
+++ bibliotheque-back-end/Models/Service/MembreService.cs
@@ -1,6 +1,41 @@
-﻿namespace bibliotheque_back_end.Models.Service;
+﻿using bibliotheque_back_end.Models.Service.Interface;
 
-public class MembreService
+namespace bibliotheque_back_end.Models.Service;
+
+public class MembreService: IMembreService
 {
-    
+    public IEnumerable<Membre> GetAllMembers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe GetMemberById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe GetMemberByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe AddMember(Membre newMember, string dataPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe UpdateMember(int id, Membre updatedMember)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employe DeleteMember(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool employeeExists(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
