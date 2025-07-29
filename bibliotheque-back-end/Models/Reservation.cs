da9f827c3ea972f74b47fc1b5f31c7b8b464c5c6
Mister-deal <max-94400@hotmail.fr>
2025-07-29 11:53:05 +0200

    refacto Membre.cs et Reservation.cs

--- bibliotheque-back-end/Models/Reservation.cs
+++ bibliotheque-back-end/Models/Reservation.cs
@@ -1,16 +1,24 @@
-﻿namespace bibliotheque_back_end.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace bibliotheque_back_end.Models
 {
     public class Reservation
     {
+        [SwaggerSchema("Id de la reservation")]
         public int Id { get; set; }
-
+        
+        [Required(ErrorMessage = "Vous devez sélectionner au moins un livre.")]
+        [MinLength(1, ErrorMessage = "La réservation doit contenir au moins un livre.")]
         public int LivreId { get; set; }
         public Livre Livre { get; set; }
 
+        [Required(ErrorMessage = "L'identifiant du membre est requis.")]
         public int MembreId { get; set; }
         public Membre Membre { get; set; }
-
-        public DateOnly DateReservation { get; set; }
-        public bool EstActive { get; set; } = true;
+        
+        [Required(ErrorMessage = "La date de réservation est requise.")]
+        public required DateOnly DateReservation { get; set; }
+        public required bool EstActive { get; set; } = false;
     }
 }
