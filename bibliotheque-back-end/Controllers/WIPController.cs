f7514f2a705f1b6ac0903127b85c74950f61fa35
Cédric L. <c.limosin@outlook.fr>
2025-08-01 12:17:22 +0200

    feat: add WIP page

---
+++ bibliotheque-back-end/Controllers/WIPController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bibliotheque_back_end.Controllers
+{
+    public class WIPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
